82ad89159aabcbaa5edf6d5d7802e356b97e344b
AhmedShaker21 <ahmedshaker23002@gmail.com>
2025-01-13 16:33:45 +0200

    Case01 : Design a Class to represent the Question Object

---
+++ ExamOOP/Question.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamOOP
+{
+    class Question
+    {
+        /// Design a Class to represent the Question Object,
+        #region Automatic Properties
+        //By Default Properties Access Modifier Public
+        string QuestionHeader { get; set; }
+        string QuestionBody { get; set; }
+        int QuestionMark { get; set; } 
+        #endregion
+
+        #region Constructor
+        public Question(string QuestionHeader, string QuestionBody, int QuestionMark)
+        {
+            this.QuestionHeader = QuestionHeader;   
+            this.QuestionBody = QuestionBody;
+            this.QuestionMark = QuestionMark;
+        }
+        #endregion
+
+        public override string ToString()
+            => $"The Question Header: {QuestionHeader}\nThe Question Body: {QuestionBody}\nThe Question Mark {QuestionMark}";
+    }
+}
