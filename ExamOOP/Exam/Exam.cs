130307367b8e24f81cb9510cc6f7e3d81d11b16b
AhmedShaker21 <ahmedshaker23002@gmail.com>
2025-01-13 18:11:15 +0200

    Case03: Initialize Exam Class With its Properties and Final and Practicle Classes are Created

---
+++ ExamOOP/Exam/Exam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamOOP.Exam
+{
+    class Exam
+    {
+        #region Automatic Properties
+        public int ExamTime { get; set; }
+        public int NumberOfQuestions { get; set; }
+
+        #endregion
+        #region Constructor
+        public Exam(int ExamTime, int NumberOfQuestions)
+        {
+            this.ExamTime = ExamTime;
+            this.NumberOfQuestions = NumberOfQuestions;
+
+        } 
+        #endregion
+
+    }
+}
